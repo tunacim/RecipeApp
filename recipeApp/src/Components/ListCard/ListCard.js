24c76e5d1bc13eb26b07574ea9ecaa0b99449454
tunacim <tunaz783@gmail.com>
2023-03-28 19:22:09 +0300

    Added Recipe Page

--- recipeApp/src/Components/ListCard/ListCard.js
+++ recipeApp/src/Components/ListCard/ListCard.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View,Text,TouchableOpacity} from "react-native";
 import styles from "./ListCard.style"
-function ListCard({item}){
+function ListCard({item,onPress}){
     return(
+        <TouchableOpacity onPress={onPress}>
         <View style={styles.container}>
             <Text style={styles.name}>{item.name}</Text>
             <Text style={styles.description}>{item.description}</Text>
         </View>
+        </TouchableOpacity>
     )
 }
 export default ListCard
--- recipeApp/src/Pages/List/List.js
+++ recipeApp/src/Pages/List/List.js
@@ -3,10 +3,11 @@ import { View,FlatList, Text} from "react-native";
 import ListCard from "../../Components/ListCard";
 import UseFetch from "../../Hooks/UseFetch";
 
-const List=()=>{
+const List=({navigation})=>{
+    const handleRecipeSelect=(id)=>{navigation.navigate("Recipe",{id})}
     const url="http://localhost:5070/recipe"
     const {data}=UseFetch(url)
-    const renderList=({item})=><ListCard item={item}></ListCard>
+    const renderList=({item})=><ListCard item={item} onPress={()=>handleRecipeSelect(item.name)}></ListCard>
     console.log(data)
     return(
        <View>     
